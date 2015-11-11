34ad18cd5d548315687a65d25e2cd197421ccdb0
plandem <plandem@gmail.com>
2015-11-11 16:46:56 +0300

    \r\n fix

--- wjet.js
+++ wjet.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
-
 var cfg = require("./package.json");
 var cmd = require("commander");
-
 var inquirer  = require('inquirer');
-
 cmd.version(cfg.version);
 cmd.command('init').description("create a new webix-jet app").action(function(){
 	require("./actions/init").run(inquirer);
+	
 });
 cmd.parse(process.argv)
